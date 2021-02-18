e491a2868fed00c64f2ae3ecc4e34dae0569cfad
lolaeva <lola.kh1994@gmail.com>
2021-02-18 11:07:50 +0300

    json-server database

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
-
+import axios from 'axios'
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number : '040-1234567' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
-
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [showAll, setShowAll] = useState('') // for filtering displayed elements
+
+  // effect-hooks
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   // event handler to the form element thatt will be called when the form is submitted
   const addPerson = (event) => {
